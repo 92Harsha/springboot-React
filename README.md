03b07b6cd066b9d7f89c436a10f63def587716c7
Harsha <harshab1992@gmail.com>
2020-03-18 16:08:11 +0530

    Spring-Boot and React Template

---
+++ README.md
@@ -0,0 +1,9 @@
+# Spring Boot and React.js using running together
+
+# Database Setup using PostgreSql
+
+~ Edit in application.properties in src/main/resources/
+
+# Run the application
+
+mvn spring-boot:run
---
+++ src/test/java/com/inoek/harsha/HarshaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inoek.harsha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarshaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
